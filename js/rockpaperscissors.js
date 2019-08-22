b6b9abd1a33d2f73b7364cb483fd6310ecbb5ac1
Kentish <kentish@gmail.com>
2019-08-22 18:59:12 +0100

    JS Init

---
+++ js/rockpaperscissors.js
@@ -0,0 +1,16 @@
+playRound()
+computerPlay()
+
+
+
+function computerPlay () {
+    // Randomly return rock, paper or scissors
+    console.log('Computer Plays')
+
+}
+
+function playRound (computerSelection , playerSelection) {
+    // Play a round of rock, paper or scissors
+    console.log('Round Start')
+}
+
