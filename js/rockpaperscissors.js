415b03b3f1bd4f2bc9f69820dd5d8463719d3dd9
Kentish <kentish@gmail.com>
2019-08-22 21:20:00 +0100

    Created Player and Computer functions

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,16 +1,31 @@
-playRound()
-computerPlay()
+const playerSelection = 'rock'
+const computerSelection = computerPlay()
+const options = ["rock" , "paper" , "scissors"]
 
 
+playRound(playerSelection, computerSelection)
 
 function computerPlay () {
     // Randomly return rock, paper or scissors
-    console.log('Computer Plays')
+    
+    
 
+    // Make a selection from the available options
+    selection = Math.floor(Math.random() * 3);
+        
+    return(options[selection])
 }
 
 function playRound (computerSelection , playerSelection) {
     // Play a round of rock, paper or scissors
     console.log('Round Start')
+    console.log(playerSelection , computerSelection) 
+    playerSelection.toLowerCase
+
+    if( )
+
 }
 
+function game() {
+
+}
