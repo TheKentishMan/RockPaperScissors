a1cf2393b92829b3ffebffa41ddb5f70fc5ed755
Kentish <kentish@gmail.com>
2019-08-26 14:28:48 +0100

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,31 +1,119 @@
-const playerSelection = 'rock'
+const playerSelection = computerPlay()
 const computerSelection = computerPlay()
-const options = ["rock" , "paper" , "scissors"]
+
+// Global Variables
+var playerScore;
+var computerScore;
+var rounds;
 
 
-playRound(playerSelection, computerSelection)
+playRound(computerSelection, playerSelection)
 
-function computerPlay () {
+function computerPlay() {
     // Randomly return rock, paper or scissors
-    
-    
+    const options = ["rock", "paper", "scissors"]
 
-    // Make a selection from the available options
     selection = Math.floor(Math.random() * 3);
-        
-    return(options[selection])
+
+    return (options[selection])
 }
 
-function playRound (computerSelection , playerSelection) {
+function playRound(computerSelection, playerSelection) {
     // Play a round of rock, paper or scissors
-    console.log('Round Start')
-    console.log(playerSelection , computerSelection) 
-    playerSelection.toLowerCase
+    console.log('Round Start');
 
-    if( )
+    switch (playerSelection) {
+        case 'rock':
+
+            switch (computerSelection) {
+                case 'rock':
+                    console.log('%cA Draw%c, both players played rock' , 'color:blue' , 'color:grey')
+                    break;
+
+                case 'scissors':
+                    console.log('Rock beats scissors: %cPlayer wins'  , 'color:blue')
+                    score('player')
+                    break;
+
+                case 'paper':
+                    console.log('Paper beats rock: %cPlayer loses', 'color:blue')
+                    break;
+            }
+
+            break;
+
+        case 'scissors':
+
+            switch (computerSelection) {
+                case 'rock':
+                    console.log('Scissor beat rock:%c Player loses' , 'color:blue')
+                    break;
+
+                case 'scissors':
+                    console.log('%cA Draw%c, both players played scissors' , 'color:blue' , 'color:grey')
+                    break;
+
+                case 'paper':
+                    console.log('Scissors beat paper:%c Player wins' , 'color:blue')
+                    score('player')
+                    break;
+            }
+
+            break;
+
+        case 'paper':
+
+            switch (computerSelection) {
+                case 'rock':
+                    console.log('Paper beats rock:%c Player wins' , 'color:blue')
+                    score('player')
+                    break;
+
+                case 'scissors':
+                    console.log('Scissors beat paper:%c Player loses' , 'color:blue')
+                    break;
+
+                case 'paper':
+                    console.log('%cA Draw%c, both players played paper', 'color:blue' , 'color:grey')
+                    break;
+            }
+
+
+            break;
+
+
+    }
+
+
 
 }
 
-function game() {
+function score(whoScored) {
+    if(whoScored == 'computer'){
+        computerScore += 1
+        console.log('Computer: ' , computerScore)
+    }
+
+    if(whoScored == 'player'){
+        playerScore += 1
+        console.log('Player:' , playerScore)
+    }
+
+    return (playerScore , computerScore)
+}
+
+function setup() {
+    // reset scores 
+    playerScore = 0;
+    computerScore = 0;
+
+    // reset rounds 
+    rounds = 0;
+}
+
+function game () {
+
+    // add a input from the user here
+    var round 
 
 }
