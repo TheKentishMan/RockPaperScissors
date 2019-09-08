164b38d0cc33dde2d93b62214533cee752925bce
Kentish <kentish@gmail.com>
2019-09-08 22:10:56 +0100

    Completed reworked the functions, now use a cleaner IF Else statement and created functions for the players

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,103 +1,82 @@
+// Commenting to help me understand
+
+
 // Global Variables
 var playerScore;
 var computerScore;
 
-const computerSelection = computerPlay()
-const computerSelection = computerPlay()
+/*
+    The functions for playerOne and Two will ultimate contain the ability to select either 
+    human or computer but for the moment they are set to computer
+*/
 
+function  playerOne (){
 
+    /*
+        Call the function and assigned it as wanted to ensure that what's written to the screen and
+        what's scored are the same
+    */
+    const playerOnePlayed = computerPlay()
 
+    document.getElementById("playerOne").innerHTML = playerOnePlayed;
+
+    return (playerOnePlayed)
+}
+
+function  playerTwo (){
+    /*
+        Call the function and assigned it as wanted to ensure that what's written to the screen and
+        what's scored are the same
+    */
+    
+    const playerTwoPlayed = computerPlay()
+    
+    document.getElementById("playerTwo").innerHTML = playerTwoPlayed;
+    
+    return (playerTwoPlayed)
+}
+
+function play(){
+    
+
+    whoWon (playerOne(), playerTwo() )
+    
+}
+
+function whoWon(playerOne , playerTwo){
+
+    /*
+        need to rework the play round function into an if statement and then write the result to the 
+        screen
+    */
+
+   document.getElementById("result").innerHTML = "";
+
+    if (playerOne == 'rock' && playerTwo == 'scissor' || 
+        playerOne == 'paper' && playerTwo == 'rock' ||
+        playerOne == 'scissors' && playerTwo == 'paper') {
+
+            document.getElementById("result").innerHTML = "player one wins";
+
+        } else if (playerOne == playerTwo ) {
+
+            document.getElementById("result").innerHTML = "draw";
+
+        } else {
+
+            document.getElementById("result").innerHTML = "player two wins";
+        }
+        
+
+}
 
 function computerPlay() {
-    // Randomly return rock, paper or scissors
+
     const options = ["rock", "paper", "scissors"]
 
     selection = Math.floor(Math.random() * 3);
-
+    
     return (options[selection])
-}
-
-
-
-function computerPlay() {
-    // Randomly return rock, paper or scissors
-    const options = ["rock", "paper", "scissors"]
-
-    selection = Math.floor(Math.random() * 3);
-
-    return (options[selection])
-}
-
-function playRound(computerSelection, playerSelection) {
-    // Play a round of rock, paper or scissors
- 
-    switch (playerSelection) {
-        case 'rock':
-
-            switch (computerSelection) {
-                case 'rock':
-                    console.log('%cA Draw%c, both players played rock' , 'color:blue' , 'color:grey')
-                    break;
-
-                case 'scissors':
-                    console.log('Rock beats scissors: %cPlayer wins'  , 'color:blue')
-                    score('player')
-                    break;
-
-                case 'paper':
-                    console.log('Paper beats rock: %cPlayer loses', 'color:blue')
-                    score('computer')
-                    break;
-            }
-
-            break;
-
-        case 'scissors':
-
-            switch (computerSelection) {
-                case 'rock':
-                    console.log('Scissor beat rock:%c Player loses' , 'color:blue')
-                    score('computer')
-                    break;
-
-                case 'scissors':
-                    console.log('%cA Draw%c, both players played scissors' , 'color:blue' , 'color:grey')
-                    break;
-
-                case 'paper':
-                    console.log('Scissors beat paper:%c Player wins' , 'color:blue')
-                    score('player')
-                    break;
-            }
-
-            break;
-
-        case 'paper':
-
-            switch (computerSelection) {
-                case 'rock':
-                    console.log('Paper beats rock:%c Player wins' , 'color:blue')
-                    score('player')
-                    break;
-
-                case 'scissors':
-                    console.log('Scissors beat paper:%c Player loses' , 'color:blue')
-                    score('computer')
-                    break;
-
-                case 'paper':
-                    console.log('%cA Draw%c, both players played paper', 'color:blue' , 'color:grey')
-                    break;
-            }
-
-
-            break;
-
-
-    }
-
-
-
 }
 
 function score(whoScored) {
@@ -114,25 +93,3 @@ function score(whoScored) {
     return (playerScore , computerScore)
 }
 
-function setup() {
-    // reset scores 
-    playerScore = 0;
-    computerScore = 0;
-}
-
-function startGame () {
-
-    setup()
-
-    // add a input from the user here
-    var rounds = 10;
-
-    for (i = 0; i < rounds; i++) {
-        console.log('%cRound', 'color:red' , i )
-        const playerSelection = computerPlay()
-        const computerSelection = computerPlay()
-        playRound(computerSelection, playerSelection)
-
-    }
-
-}
